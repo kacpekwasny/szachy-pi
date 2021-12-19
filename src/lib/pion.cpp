5d057eef9c9f33a12db82eadd8a208da1f34ec4d
Joanna Kitlinska <jkitlinska@student.agh.edu.pl>
2021-12-19 23:23:57 +0100

    dozwolone ruchy krolowa i goniec

--- src/lib/pion.cpp
+++ src/lib/pion.cpp
@@ -8,7 +8,7 @@
 
 bool Pion::move(std::string pole) { return false; }
 
-bool ruchPionkiemDozwolony(kp wiersz, kp kolumna){
+bool Pion::ruchPionkiemDozwolony(kp wiersz, kp kolumna){
 	if(kolumna_ - this-> kolumna != 0 || wiersz - this-> wiersz_ != 1){
 		/* wykluczam mozliwosc poruszania sie po przekatnych, na boki,
 		 * do tylu lub o wiecej niz jedno pole do przodu */
@@ -17,7 +17,7 @@ bool ruchPionkiemDozwolony(kp wiersz, kp kolumna){
 	return true;
 }
 
-bool ruchWiezaDozwolony(kp wiersz, kp kolumna){
+bool Pion::ruchWiezaDozwolony(kp wiersz, kp kolumna){
 	if(this->kolumna_ - kolumna != 0 && this-> wiersz_ - wiersz != 0){
 		/* wykluczam mozliwosc poruszania sie po przekatnych */
 		return false;
@@ -25,10 +25,26 @@ bool ruchWiezaDozwolony(kp wiersz, kp kolumna){
 	return true;
 }
 
-bool ruchSkoczkaDozwolony(kp wiersz, kp kolumna){
+bool Pion::ruchSkoczkaDozwolony(kp wiersz, kp kolumna){
 	if(abs(this->kolumna_ - kolumna) == 3 && abs(this->wiersz_ - wiersz) == 1){
 		return true;
 	} else if(abs(this->wiersz_ - wiersz) == 3 && abs(this->kolumna_ - kolumna) == 1){
+		return true;
+	}
+	return false;
+}
+
+bool Pion::ruchGoncaDozwolony(kp wiersz, kp kolumna){
+	if(abs(this->kolumna_ - kolumna) == abs(this->wiersz_ - wiersz){
+		return true;
+	}
+	return false;
+}
+
+bool Pion::ruchKrolowejDozwolony(kp wiersz, kp kolumna){
+	if(abs(this->kolumna_ - kolumna) == abs(this->wiersz_ - wiersz){
+		return true;
+	} else if(abs(this->kolumna_ - kolumna) == 0 || abs(this->wiersz_ - wiersz) == 0){
 		return true;
 	}
 	return false;
