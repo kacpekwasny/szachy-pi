c634236c7dc679439364bd8bcac0c6cd5ae707aa
Mateusz Walas <410851@student.agh.edu.pl>
2021-12-19 19:42:55 +0100

    główna baza programu .cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "def_typow.hpp"
+#include "oprawaTekstowa.hpp"
+#include "plansza.hpp"
+
+int main() {
+
+    startProgram();
+    createBoard();
+    generatePositions();
+    // generatePositionX();
+    // generatePositionY();
+    setFigures();
+    showBoard();
+
+    std::cout << "Podaj pole figury, ktora chcesz sie ruszyc." << std::endl;
+    std::string input;  
+    std::cin >> input;
+
+    setText(input);
+
+return 0;
+}
