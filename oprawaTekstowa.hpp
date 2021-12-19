32e6c8e149074d0e31d559b8bbf3993f15e1e6e9
Mateusz Walas <410851@student.agh.edu.pl>
2021-12-19 19:41:50 +0100

    interfejs.hpp

---
+++ oprawaTekstowa.hpp
@@ -0,0 +1,14 @@
+#pragma once
+//
+// Created by mateusz on 19.12.2021.
+//
+
+#include <string>
+
+void startProgram();
+
+	//funkcja zaraz po starcie programu. Powitanie tekstowe i zasady gry.
+
+void setText(std::string text);
+
+	//funkcja odpowiedzialna za wszystkie powiadomienia do uzytkownika, np wybierz pole; to pole jest zajête itd.
