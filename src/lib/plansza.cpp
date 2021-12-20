54e0ab78ca4854789309b38949a5fffd003c6044
jarek <jarekomsta@wp.pl>
2021-12-20 17:59:12 +0100

    render update

--- src/lib/plansza.cpp
+++ src/lib/plansza.cpp
@@ -2,25 +2,22 @@
 // Created by jarek on 17.12.2021.
 //
 #include "Plansza.hpp"
+
 #include <regex>
 
 Plansza::Plansza(kp iloscWierszy, kp iloscKolumn) {
     // inicjalizacja pol (planszy de fakto)
+    bool poleBiale = false;
     for (kp w = 0; w < iloscWierszy; w++) {
         std::vector<Pole *> wiersz;
 
         for (kp k = 0; k < iloscKolumn; k++) {
-            Pole *p;
-            p->isZajete = false;
-            p->kolumna = k;
-            p->wiersz = w;
+            Pole *p = &Pole(w, k, poleBiale);
+            poleBiale = !poleBiale;
             wiersz.push_back(p);
         }
         pola.push_back(wiersz);
     }
-}
-Plansza::Plansza() :Plansza(8,8) {
-
 }
 
 Plansza::~Plansza() {
