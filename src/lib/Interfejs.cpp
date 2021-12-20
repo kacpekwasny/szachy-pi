7ada914b2dfa6c42c2ca1ee64a00bd30ae3fdd29
jarek <jarekomsta@wp.pl>
2021-12-20 11:50:29 +0100

    przebudowa set text w celu lepszej rozszezalnosci programu

--- src/lib/Interfejs.cpp
+++ src/lib/Interfejs.cpp
@@ -34,6 +34,10 @@ void Interfejs::startProgram() {  //zaraz po starcie programu
     std::cout << "exit";
     SetConsoleTextAttribute(hConsole, 7); //color white
     std::cout << " - zakonczenie gry i wyjscie z programu." << std::endl;
+    SetConsoleTextAttribute(hConsole, 2); //color green
+    std::cout << "info";
+    SetConsoleTextAttribute(hConsole, 7); //color white
+    std::cout << " - informacja o możliwych ruchach oraz krótkie wprowadzenie." << std::endl;
     std::cout << std::endl;
     std::cout << "Aby poruszac sie figurami po planszy posluguj sie notacja szachowa np. d5." << std::endl;
     std::cout << "Pamiętaj, kolejnosc wpisywania ma znaczenie - najpierw litera, potem liczba." << std::endl;
@@ -45,14 +49,12 @@ void Interfejs::startProgram() {  //zaraz po starcie programu
 }
 void Interfejs::setText(std::string) {
 
-    if (text == "reset" || text == "exit") {
-        if (text == "reset") {
-
+    switch(text){
+        case "reset":
             std::string decyzja;
             std::cout << std::endl;
             std::cout << "czy chcesz zresetowac i zaczac od nowa? [t/n]" << std::endl;
             std::cin >> decyzja;
-
             if (decyzja == "t") {
 
                 system("cls");
@@ -71,9 +73,8 @@ void Interfejs::setText(std::string) {
                 std::cin >> new_input;
                 setText(new_input);
             }
-        }
-
-        if (text == "exit") {
+            break;
+        case "exit":
             system("cls");
             std::cout << std::endl;
             SetConsoleTextAttribute(hConsole, 12); //color red
@@ -86,50 +87,51 @@ void Interfejs::setText(std::string) {
             std::cout << "Asia, Dominik, Hubert, Jarek, Julian, Kacper & Mateusz." << std::endl;;
             std::cout << std::endl;
             system("pause");
-        }
-    }
-    else {
-        /*
+            break;
+        default:
 
-        kp=text;  //jakos sie to rozwiaze xd
+            /*
 
-        if (CzyWybranePoleNalezyDoPlanszy(kp) == true) {
+            kp=text;  //jakos sie to rozwiaze xd
 
-            if (CzyNaPoluStoiJakasFigura(kp) == true) {
-                std::cout << "Wybierz pole, na ktore chcesz sie ruszyc. << std::endl;
-                std::cin >> kp;
-                if (CzyWybranePoleNalezyDoPlanszy(kp) == true) {
+            if (CzyWybranePoleNalezyDoPlanszy(kp) == true) {
 
-                    if (CzyNaPoluStoiJakasFigura(kp) == false) {
+                if (CzyNaPoluStoiJakasFigura(kp) == true) {
+                    std::cout << "Wybierz pole, na ktore chcesz sie ruszyc. << std::endl;
+                    std::cin >> kp;
+                    if (CzyWybranePoleNalezyDoPlanszy(kp) == true) {
 
-                        if (CzyMozeSieTamRuszyc == true ) {
-                            //funkcja replace? - rusz tam figure
+                        if (CzyNaPoluStoiJakasFigura(kp) == false) {
+
+                            if (CzyMozeSieTamRuszyc == true ) {
+                                //funkcja replace? - rusz tam figure
+                            }
+                            else {
+                                std::cout << "Wybrana figura nie moze sie tam ruszyc, wpisz koncowa wspolrzedna jeszcze raz." << std::endl;
+                            }
                         }
                         else {
-                            std::cout << "Wybrana figura nie moze sie tam ruszyc, wpisz koncowa wspolrzedna jeszcze raz." << std::endl;
+                            std::cout << "Wybrane pole juz jest zajete. Wpisz koncowa wspolrzedna jeszcze raz." << std::endl;
                         }
                     }
                     else {
-                        std::cout << "Wybrane pole juz jest zajete. Wpisz koncowa wspolrzedna jeszcze raz." << std::endl;
+                        std::cout << "Wybrane pole nie nalezy do planszy. Wpisz koncowa wspolrzedna jeszcze raz." << std::endl;
+                        funkcja??
                     }
                 }
                 else {
-                    std::cout << "Wybrane pole nie nalezy do planszy. Wpisz koncowa wspolrzedna jeszcze raz." << std::endl;
-                    funkcja??
+                    std::cout << "To pole jest puste i nie stoi na nim zadna figura, wpisz pierwsza wspolrzedna jeszcze raz." << std::endl;
+                    std::string new_input;
+                    std::cin >> new_input;
+                    setText(new_input);
                 }
             }
-            else {
-                std::cout << "To pole jest puste i nie stoi na nim zadna figura, wpisz pierwsza wspolrzedna jeszcze raz." << std::endl;
-                std::string new_input;
-                std::cin >> new_input;
-                setText(new_input);
-            }
-        }
-        else { */
-        std::cout << "Wybrane pole nie nalezy do planszy. Wpisz pierwsza wspolrzedna jeszcze raz." << std::endl;
-        std::string new_input;
-        std::cin >> new_input;
-        setText(new_input);
-        //}
+            else { */
+            std::cout << "Wybrane pole nie nalezy do planszy. Wpisz pierwsza wspolrzedna jeszcze raz." << std::endl;
+            std::string new_input;
+            std::cin >> new_input;
+            setText(new_input);
+            //}
+
     }
 }
